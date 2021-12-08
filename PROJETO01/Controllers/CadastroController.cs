58175d767ba885b956268e5895a5b610b07fd632
Lucas Gabriel Oliveira da Silva <040161@campus1.uniaraxa.edu.br>
2021-12-07 22:03:03 -0300

    atualizar

---
+++ PROJETO01/Controllers/CadastroController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PROJETO01.Dados.EntityFramework;
+using PROJETO01.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PROJETO01.Controllers
+{
+    public class CadastroController : Controller
+    {
+ 
+            public IActionResult Index(string nome, string endereço, string cpf, int cep, int numero, string bairro, string complemento      )
+            {
+                var objeto = new Cadastro();
+                objeto.Nome = nome;
+                objeto.Endereço = endereço;
+                objeto.Cpf = cpf;
+                objeto.Cep = cep;
+                objeto.Numero = numero;
+                objeto.Bairro = bairro;
+                objeto.Complemento = complemento;
+
+                return View(objeto);
+            }
+
+            [HttpGet]
+            public IActionResult Adicionar()
+            {
+                return View();
+            }
+
+            public IActionResult AdicionarConfirmacao(Cadastro cadastro)
+            {
+                var db = new Contexto();
+
+                var obj = db.Cadastro.FirstOrDefault(f => f.Nome == cadastro.Nome);
+
+                if (obj == null)
+                {
+                    db.Cadastro.Add(cadastro);
+                }
+                else
+                {
+                    obj.Nome = cadastro.Nome;
+                    db.Cadastro.Update(obj);
+                }
+
+                db.SaveChanges();
+
+                return RedirectToAction("Listar");
+            }
+
+            [HttpGet]
+            public IActionResult Editar(string uf)
+            {
+                var db = new Contexto();
+                var estado = db.Estado.First(item => item.UF == uf);
+                return View("Adicionar", estado);
+            }
+
+            public IActionResult Listar()
+            {
+
+                //SELECT * FROM Estado
+                var listaDeEstados = new Contexto().Estado.ToList();
+
+                return View(listaDeEstados);
+            }
+
+            public IActionResult Excluir(string uf)
+            {
+                var db = new Contexto();
+                var estado = db.Estado.First(f => f.UF == uf);
+                db.Estado.Remove(estado);
+                db.SaveChanges();
+
+                return RedirectToAction("Listar");
+            }
+        }
+    }
+
---
+++ PROJETO01/Controllers/CorridaController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PROJETO01.Dados.EntityFramework;
+using PROJETO01.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PROJETO01.Controllers
+{
+    public class CorridaController : Controller
+    {
+
+        public IActionResult Index(int CodMotorista, int CodPassageiro, string EstadoID, string Cidade, string avaliacao)
+        {
+            var objeto = new Corrida();
+            objeto.CodMotorista = CodMotorista;
+            objeto.CodPassageiro = CodPassageiro;
+            objeto.EstadoID = EstadoID;
+            objeto.Cidade = Cidade;
+            objeto.Avaliacao = avaliacao;
+            return View(objeto);
+        }
+
+        [HttpGet]
+        public IActionResult Adicionar()
+        {
+            return View();
+        }
+
+        public IActionResult AdicionarConfirmacao(Corrida corrida)
+        {
+            var db = new Contexto();
+
+            var obj = db.Corrida.FirstOrDefault(f => f.CodMotorista == corrida.CodMotorista);
+
+            if (obj == null)
+            {
+                db.Corrida.Add(corrida);
+            }
+            else
+            {
+                obj.CodMotorista = corrida.CodMotorista;
+                db.Corrida.Update(obj);
+            }
+
+            db.SaveChanges();
+
+            return RedirectToAction("Listar");
+        }
+
+        [HttpGet]
+        public IActionResult Editar(string uf)
+        {
+            var db = new Contexto();
+            var estado = db.Estado.First(item => item.UF == uf);
+            return View("Adicionar", estado);
+        }
+
+        public IActionResult Listar()
+        {
+
+            //SELECT * FROM Estado
+            var listaDeEstados = new Contexto().Estado.ToList();
+
+            return View(listaDeEstados);
+        }
+
+        public IActionResult Excluir(string uf)
+        {
+            var db = new Contexto();
+            var estado = db.Estado.First(f => f.UF == uf);
+            db.Estado.Remove(estado);
+            db.SaveChanges();
+
+            return RedirectToAction("Listar");
+        }
+    }
+}
